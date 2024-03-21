4082397c22b3363a99f53c56e065073d598275b6
elkherchi <22025@supnum.mr>
2024-03-21 21:45:50 +0000

    changement

--- connect.js
+++ connect.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
 const con = mysql.createConnection({
-  host: 'ssc.cluster-c7428qqac1q4.eu-west-3.rds.amazonaws.com',
+  host: ' databasessc.c7428qqac1q4.eu-west-3.rds.amazonaws.com',
   port:'3306',
   user: 'admin',
-  password: 'admin1234',
-  database: 'ssc',
+  password: 'supnum1234',
+  database: 'sscsupnum'
 });
 
 con.connect((err) => {
